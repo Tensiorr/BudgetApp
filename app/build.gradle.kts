0acccec141ae394793ff9a39b485bf32edb93ef7
Tensiorr <tesiorowski.marek@gmail.com>
2026-02-15 02:31:07 +0100

    Fix Gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,20 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.tensiorr.budgetapp"
-    compileSdk = 34
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
         applicationId = "com.tensiorr.budgetapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -28,8 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
--- app/src/main/java/com/tensiorr/budgetapp/MainActivity.kt
+++ app/src/main/java/com/tensiorr/budgetapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tensiorr.budgetapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +12,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.tensiorr.budgetapp.data.database.AppDatabase
 import com.tensiorr.budgetapp.ui.theme.BudgetAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val db = AppDatabase.getDatabase(this)
+        val dao = db.transactionDao()
+        lifecycleScope.launch {
+            dao.getAllTransactions().collect { transactions ->
+                Log.d("MainActivity", "Liczba transakcji: ${transactions.size}")
+            }
+        }
         setContent {
             BudgetAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
